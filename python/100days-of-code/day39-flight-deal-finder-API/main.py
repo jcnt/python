c6593c57c2b672f48794de9c901a5f673b897545
Jacint Juhasz <yyaazz@gmail.com>
2024-04-09 23:51:24 +0200

    Tue Apr  9 23:51:24 CEST 2024

--- python/100days-of-code/day39-flight-deal-finder-API/main.py
+++ python/100days-of-code/day39-flight-deal-finder-API/main.py
@@ -1,16 +1,26 @@
 import data_manager
 import flight_search
 
-sheet = data_manager.DataManager()
-sheet_data = sheet.get_reqs()['prices']
+# sheet = data_manager.DataManager()
+# sheet_data = sheet.get_reqs()['prices']
 
 search = flight_search.FlightSearch()
 
-for i in sheet_data: 
-    if i['iataCode'] == "": 
-        print(i['city'], i['id'])
-        iata = search.get_iata(i['city'])
-        print(iata)
-        sheet.update_iata(iata, i['id'])
+#for i in sheet_data: 
+#    if i['iataCode'] == "": 
+#        print(i['city'], i['id'])
+#        iata = search.get_iata(i['city'])
+#        print(iata)
+#        sheet.update_iata(iata, i['id'])
+
+#for i in sheet_data:
+#    hit = search.flight_search(i['iataCode'])
+#    print(hit)
 
 
+hit = search.flight_search('LAX')
+print(hit)
+
+
+
+    
