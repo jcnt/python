433affe771e3a809da0b69d1ee4bbd80267d6589
Jacint Juhasz <yyaazz@gmail.com>
2024-04-10 22:40:00 +0200

    Wed Apr 10 22:40:00 CEST 2024

--- python/100days-of-code/day39-flight-deal-finder-API/main.py
+++ python/100days-of-code/day39-flight-deal-finder-API/main.py
@@ -1,26 +1,22 @@
 import data_manager
 import flight_search
+import notification_manager
 
-# sheet = data_manager.DataManager()
-# sheet_data = sheet.get_reqs()['prices']
+sheet = data_manager.DataManager()
+sheet_data = sheet.get_reqs()['prices']
 
 search = flight_search.FlightSearch()
 
-#for i in sheet_data: 
-#    if i['iataCode'] == "": 
-#        print(i['city'], i['id'])
-#        iata = search.get_iata(i['city'])
-#        print(iata)
-#        sheet.update_iata(iata, i['id'])
+for i in sheet_data: 
+    if i['iataCode'] == "": 
+        print(i['city'], i['id'])
+        iata = search.get_iata(i['city'])
+        print(iata)
+        sheet.update_iata(iata, i['id'])
 
-#for i in sheet_data:
-#    hit = search.flight_search(i['iataCode'])
-#    print(hit)
-
-
-hit = search.flight_search('LAX')
-print(hit)
+for i in sheet_data:
+    hit = search.flight_search(i['iataCode'])
 
 
-
-    
+message = notification_manager.NotificationManager()
+message.sendtext(hit)
--- python/100days-of-code/day39-flight-deal-finder-API/notification_manager.py
+++ python/100days-of-code/day39-flight-deal-finder-API/notification_manager.py
@@ -1,3 +1,21 @@
+from twilio.rest import Client
+from os import environ
+
+SID = environ["TWILIO_SID"]
+TOKEN = environ["TWILIO_TOKEN"]
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def sendtext(self, body):
+        print(body)
+
+#        client = Client(SID, TOKEN)
+#        message = client.messages.create(
+#            from_="+16815395838",
+#            body=body,
+#            to='+36303434808'
+#        )
+#
+#        print(message.sid)
+
