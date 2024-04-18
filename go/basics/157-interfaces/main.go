726cb75a70dd6f89c3135e1ec1bfea6614bc5bd7
Jacint Juhasz <yyaazz@gmail.com>
2024-04-18 23:42:13 +0200

    Thu Apr 18 23:42:13 CEST 2024

---
+++ go/basics/157-interfaces/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	length float64
+	width float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.width
+}
+
+func (c circle) area() float64 {
+	return math.Pow(c.radius, 2) * math.Pi
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) float64 {
+	return s.area()
+}
+
+func main(){
+	c1 := circle {
+		radius: 4,
+	}
+
+	s1 := square {
+		length: 5,
+		width: 8,
+	}
+
+	fmt.Println(info(c1))
+	fmt.Println(info(s1))
+
+
+}
