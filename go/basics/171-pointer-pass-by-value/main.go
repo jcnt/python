688313601dd6d74999f36e8321cb6de2c7107fc1
Jacint Juhasz <yyaazz@gmail.com>
2024-04-25 08:15:13 +0200

    Thu Apr 25 08:15:13 CEST 2024

--- go/basics/171-pointer-pass-by-value/main.go
+++ go/basics/171-pointer-pass-by-value/main.go
@@ -6,6 +6,14 @@ func intDelta(n *int) {
 	*n = 43
 }
 
+func sliceDelta(ii []int) {
+	ii[0] = 99
+}
+
+func mapDelta(md map[string]int, s string) {
+	md[s] = 43
+}
+
 func main() {
 	a := 42
 	fmt.Println(a)
@@ -14,4 +22,12 @@ func main() {
 
 	xi := []int{1, 2, 3, 4}
 	fmt.Println(xi)
+	sliceDelta(xi)
+	fmt.Println(xi)
+
+	m := make(map[string]int)
+	m["James"] = 42
+	fmt.Println(m["James"])
+	mapDelta(m, "James")
+	fmt.Println(m["James"])
 }
