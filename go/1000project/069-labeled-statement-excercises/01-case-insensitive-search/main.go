3753e57a28ade65f5cd794f28c704c1d3ffd02b4
Jacint Juhasz <yyaazz@gmail.com>
2024-05-20 01:55:23 +0400

    Mon May 20 01:55:23 +04 2024

---
+++ go/1000project/069-labeled-statement-excercises/01-case-insensitive-search/main.go
@@ -0,0 +1,48 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Case Insensitive Search
+//
+//  Allow for case-insensitive searching
+//
+// EXAMPLE
+//  Let's say that the user runs the program like this:
+//    go run main.go LAZY
+//
+//  Or like this: go run main.go lAzY
+//  Or like this: go run main.go lazy
+//
+//  For all cases above, the program should find
+//  the "lazy" keyword.
+// ---------------------------------------------------------
+
+const corpus = "lazy cat jumps again and again and again"
+
+func main() {
+	words := strings.Fields(corpus)
+	query := os.Args[1:]
+
+	for _, q := range query {
+		q = strings.ToLower(q)
+		for i, w := range words {
+			if q == w {
+				fmt.Printf("#%-2d: %q\n", i+1, w)
+				break
+			}
+		}
+	}
+}
