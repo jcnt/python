6d2537ff19f5d2d79f18e0a2e97e12f27c0889f8
Jacint Juhasz <yyaazz@gmail.com>
2024-08-17 10:08:48 +0200

    Sat Aug 17 10:08:48 CEST 2024

--- python/100days-of-code/day45-beautiful-soop/main.py
+++ python/100days-of-code/day45-beautiful-soop/main.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 response = requests.get("https://news.ycombinator.com/news", verify=False)
 
@@ -11,27 +11,27 @@ article_tag = soup.find(name="a")
 article_text = article_tag.getText()
 print(article_text)
 
-# 
-# This is the Beautiful Soup intro with examples: 
+#
+# This is the Beautiful Soup intro with examples:
 #
 #
 #
-#with open("python/100days-of-code/day45-beautiful-soop/website.html") as file: 
+# with open("python/100days-of-code/day45-beautiful-soop/website.html") as file:
 #    content = file.read()
 #
-#soup = BeautifulSoup(content, "html.parser")
+# soup = BeautifulSoup(content, "html.parser")
 #
-#print(soup.title.string)
-#print(soup.prettify())
-#print("------------")
+# print(soup.title.string)
+# print(soup.prettify())
+# print("------------")
 #
-#all_anchor_tags = soup.find_all(name="a")
-#print(all_anchor_tags)
-#print("------------")
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+# print("------------")
 #
-#for i in all_anchor_tags:
+# for i in all_anchor_tags:
 #    print(i.getText())
-#print("------------")
+# print("------------")
 #
-#heading = soup.find(name="h1", id="name")
-#print(heading)
+# heading = soup.find(name="h1", id="name")
+# print(heading)
