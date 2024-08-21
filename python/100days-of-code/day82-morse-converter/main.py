b7a874b1c22e49d74c94cb191b04c8b3f6de7cde
Jacint Juhasz <yyaazz@gmail.com>
2024-08-21 23:53:11 +0200

    Wed Aug 21 23:53:11 CEST 2024

--- python/100days-of-code/day82-morse-converter/main.py
+++ python/100days-of-code/day82-morse-converter/main.py
@@ -1,19 +1,34 @@
 # assignment porgram for python scripting
-# get a string from input and translate it to morse signs.
+# Day 82: get a string from input and translate it to morse signs.
 
 import csv
 import os
 
 with open("morse.csv", newline="") as file:
     data = csv.reader(file)
-    morse = list(data)
+    #     morse = list(data)    # this would do a list of lists
+    abcmorse = {rows[0]: rows[1] for rows in data}
 
 
 def mreturn(c):
-    for i in range(36):
-        if c == morse[i][0]:
-            return morse[i][1]
+    morse = ""
+    for i in range(len(c)):
+        if c[i].upper() not in abcmorse:
+            return f'Invalid character, "{c[i]}" does not have a Morse code...'
+        morse += abcmorse[f"{c[i].upper()}"]
+        morse += "   "
+    return morse
 
 
-g = mreturn("C")
-print(g)
+running = True
+
+while running:
+    os.system("clear")
+    uval = input("Please enter a string to get its Morse code: ")
+    print("\nMorse code: ")
+    print(mreturn(uval))
+    again = input("\nNext string or Quit? N/Q: ")
+
+    if again.upper() == "Q":
+        running = False
+        print("\nBye!\n")
