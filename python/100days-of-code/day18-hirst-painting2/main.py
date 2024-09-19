75d433e51950d7e2d8a549dfe9f7a2d8b5cb7088
Jacint Juhasz <yyaazz@gmail.com>
2024-09-19 18:43:58 +0200

    Lazy commit Thu Sep 19 18:43:58 CEST 2024

--- python/100days-of-code/day18-hirst-painting2/main.py
+++ python/100days-of-code/day18-hirst-painting2/main.py
@@ -1,7 +1,8 @@
 import random
+import turtle
 
 import colorgram
-import turtle
+
 turtle.colormode(255)
 tim = turtle.Turtle()
 
@@ -9,14 +10,33 @@ tim.penup()
 tim.setpos(-300, -50)
 tim.pendown()
 
-colorlist = [(84, 254, 155), (173, 146, 118), (254, 250, 254), (245, 39, 191), (158, 107, 56), (2, 1, 176), (151, 54, 251), (221, 254, 101), (253, 146, 193), (3, 87, 176), (249, 1, 246), (35, 34, 253), (1, 213, 212), (249, 0, 0), (254, 147, 146), (253, 71, 70), (244, 248, 254), (39, 249, 42)]
+colorlist = [
+    (84, 254, 155),
+    (173, 146, 118),
+    (254, 250, 254),
+    (245, 39, 191),
+    (158, 107, 56),
+    (2, 1, 176),
+    (151, 54, 251),
+    (221, 254, 101),
+    (253, 146, 193),
+    (3, 87, 176),
+    (249, 1, 246),
+    (35, 34, 253),
+    (1, 213, 212),
+    (249, 0, 0),
+    (254, 147, 146),
+    (253, 71, 70),
+    (244, 248, 254),
+    (39, 249, 42),
+]
 
 
 # colors = colorgram.extract("image.jpg", 20)
 
-#colorlist = []
+# colorlist = []
 #
-#for i in range(20):
+# for i in range(20):
 #    act_color = colors[i]
 #    r = act_color.rgb.r
 #    g = act_color.rgb.g
@@ -24,11 +44,11 @@ colorlist = [(84, 254, 155), (173, 146, 118), (254, 250, 254), (245, 39, 191), (
 #    rgb = (r, g, b)
 #    colorlist.append(rgb)
 
-#print(colorlist)
+# print(colorlist)
 #
-#tim.pensize(50)
+# tim.pensize(50)
 #
-#for j in range(6):
+# for j in range(6):
 #    tim.pendown()
 #    tim.color(colorlist[j])
 #    tim.forward(50)
@@ -40,14 +60,14 @@ tim.pensize(20)
 
 for i in range(10):
     for j in range(10):
-        act_num = random.randint(0, collen-1)
+        act_num = random.randint(0, collen - 1)
         act_color = colorlist[act_num]
         tim.color(act_color)
         tim.pendown()
         tim.forward(1)
         tim.penup()
         tim.forward(50)
-    tim.goto(-300, i*50)
+    tim.goto(-300, i * 50)
 
 tim.hideturtle()
 
