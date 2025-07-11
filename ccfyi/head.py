b78516feed86cc24eca624020e6d7e534fe0cf4d
Jacint Juhasz <yyaazz@gmail.com>
2025-07-12 01:03:23 +0200

    Lazy commit Sat Jul 12 01:03:23 CEST 2025

--- ccfyi/head.py
+++ ccfyi/head.py
@@ -6,6 +6,7 @@ write a program that does the same as the unix "head"
 """
 
 import sys
+from os import walk
 
 
 def read_stdin():
@@ -16,4 +17,34 @@ def read_stdin():
     return stdin
 
 
+def noarg():
+    print("noarg")
+
+
+def argn():
+    print("argn")
+
+
+def argc():
+    print("argc")
+
+
 print(len(sys.argv))
+print(sys.argv[0])
+
+# 1 -> piped into
+# 2 -> no arg just filename
+
+if len(sys.argv) == 1:
+    print("1")
+    # no arg, piped, head -10
+
+if len(sys.argv) == 2:
+    print("2")
+    # head filename
+    # or
+    # head -arg piped
+
+if len(sys.argv) == 3:
+    print("3")
+    # head -arg filename
