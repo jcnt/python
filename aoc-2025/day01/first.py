f1b93b72ddc69f3975d3e09ed1604b7e934069d3
Jacint Juhasz <yyaazz@gmail.com>
2026-01-07 21:56:36 +0100

    Lazy commit Wed Jan  7 21:56:36 CET 2026

--- aoc-2025/day01/first.py
+++ aoc-2025/day01/first.py
@@ -3,11 +3,29 @@ Advent of Code, 2025. Day 1, part 1.
 
 """
 
-with open("example", "r") as file:
+with open("input", "r") as file:
     f = file.read().splitlines()
+
+dial = 50
+count = 0
 
 for i in f:
     if i[0] == "L":
-        print("left")
+        dial = dial - int(i[1:]) % 100
+        if dial == 0:
+            count += 1
+        if dial < 0:
+            dial += 100
+        print("L", i[1:], dial, count)
     elif i[0] == "R":
-        print("right")
+        dial += int(i[1:]) % 100
+        if dial == 100:
+            dial = 0
+            count += 1
+        if dial > 100:
+            dial = dial % 100
+            if dial == 0:
+                count += 1
+        print("R", i[1:], dial, count)
+
+print(count)
