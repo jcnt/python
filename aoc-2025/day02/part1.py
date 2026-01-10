d2f9ebe43935bc329894fe2f43ba7e89f0e3b60b
Jacint Juhasz <yyaazz@gmail.com>
2026-01-10 15:34:19 +0100

    Lazy commit Sat Jan 10 15:34:19 CET 2026

--- aoc-2025/day02/part1.py
+++ aoc-2025/day02/part1.py
@@ -3,13 +3,21 @@ Advent of Code, 2025. Day 2, part 1.
 
 """
 
-with open("example", "r") as file:
+with open("input", "r") as file:
     f = file.read().split(",")
 
+counter = 0
 
 for ranges in f:
     cr = ranges.split("-")
     l = list(range(int(cr[0]), int(cr[1]) + 1))
     for code in l:
-        if len(code) % 2 == 0:
-            print(code)
+        scode = str(code)
+        clen = len(str(code))
+        if clen % 2 == 0:
+            f = scode[0 : int(clen / 2)]
+            s = scode[int(clen / 2) : clen]
+            if f == s:
+                counter += code
+
+print(counter)
