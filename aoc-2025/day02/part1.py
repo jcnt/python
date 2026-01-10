76ebd9e0b9f3fb36dbac4dfeacef98d569b80ec2
Jacint Juhasz <yyaazz@gmail.com>
2026-01-10 15:18:08 +0100

    Lazy commit Sat Jan 10 15:18:08 CET 2026

--- aoc-2025/day02/part1.py
+++ aoc-2025/day02/part1.py
@@ -10,4 +10,6 @@ with open("example", "r") as file:
 for ranges in f:
     cr = ranges.split("-")
     l = list(range(int(cr[0]), int(cr[1]) + 1))
-    print(l)
+    for code in l:
+        if len(code) % 2 == 0:
+            print(code)
