d7bdf50a6ae0449dfb102de3cfcd7cc722078373
Jacint Juhasz <yyaazz@gmail.com>
2026-01-11 22:00:36 +0100

    Lazy commit Sun Jan 11 22:00:36 CET 2026

--- aoc-2025/day02/part2.py
+++ aoc-2025/day02/part2.py
@@ -18,20 +18,22 @@ for ranges in f:
 
     for code in l:
         for i in range(1, int(maxlen / 2) + 1):
-            lcode = len(str(code))
-            print("lcode", lcode)
-            if lcode % i == 0:
-                tl = []
-                for j in range(0, lcode, int(lcode / i)):
-                    tl.append(str(code)[j : j + int(lcode / i)])
-                    print("tl", tl)
-                print(tl)
-                isEqual = True
-                for k in range(1, len(tl)):
-                    if tl[0] != tl[k]:
-                        isEqual = False
-                print(isEqual)
-                if isEqual == True:
-                    counter += code
+            print(i)
 
+            #            lcode = len(str(code))
+            #            print("lcode", lcode)
+            #            if lcode % i == 0:
+            #                tl = []
+            #                for j in range(0, lcode, int(lcode / i)):
+            #                    tl.append(str(code)[j : j + int(lcode / i)])
+            #                    print("tl", tl)
+            #                print(tl)
+            #                isEqual = True
+            #                for k in range(1, len(tl)):
+            #                    if tl[0] != tl[k]:
+            #                        isEqual = False
+            #                print(isEqual)
+            #                if isEqual == True:
+            #                    counter += code
+            #
 print(counter)
