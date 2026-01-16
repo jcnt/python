46ba69de6f77a6179bc554947f119ed532bdf98e
Jacint Juhasz <yyaazz@gmail.com>
2026-01-16 17:43:23 +0100

    working python AOC 2025 day04 part 1

--- aoc-2025/day04/part1.py
+++ aoc-2025/day04/part1.py
@@ -3,16 +3,34 @@ Advent of Code, 2025. Day 3, part 2.
 
 """
 
-with open("example", "r") as file:
+with open("input", "r") as file:
     f = file.read().split("\n")
 
+f.remove("")
 print(f)
 
+result = 0
+
+print("len/f", len(f))
+
 for row in range(len(f)):
+    print("lenfrow", len(f[row]))
     for item in range(len(f[row])):
         print(row, item)
         print(f[row][item])
-        for i in range(row - 1, row + 2):
-            for j in range(item - 1, item + 2):
-                if i >= 0 and i < len(f[row]) and j >= 0 and j < len(f):
-                    print(f[row][item], i, j, f[i][j], "\n")
+        if f[row][item] == "@":
+            counter = 0
+            for i in range(row - 1, row + 2):
+                for j in range(item - 1, item + 2):
+                    if i >= 0 and i < len(f[row]) and j >= 0 and j < len(f):
+                        if i == row and j == item:
+                            print("this is me")
+                        else:
+                            print(i, j, f[i][j], row, item)
+                            if f[i][j] == "@":
+                                counter += 1
+            print("counter", counter, "\n")
+            if counter < 4:
+                result += 1
+
+print("final result = ", result)
