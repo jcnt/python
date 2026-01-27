db16d393e620a2caee4c62f003273f1256f7d176
Jacint Juhasz <yyaazz@gmail.com>
2026-01-27 22:56:45 +0100

    Advent of Code, 2025 day05, part 2 final working version

--- aoc-2025/day05/part2.py
+++ aoc-2025/day05/part2.py
@@ -3,14 +3,15 @@ Advent of Code, 2025. Day 5, part 2.
 
 """
 
-with open("example", "r") as file:
+with open("input", "r") as file:
     f = file.read().split("\n")
 
 print(f)
 
 fresh = []
-current = 0
-counter = 0
+clist = [0, 0]
+counter = []
+fcount = 0
 
 for i in f:
     if "-" in i:
@@ -23,16 +24,21 @@ fresh.sort()
 print("fresh items:", fresh)
 
 for l in fresh:
-    if l[0] >= current:
-        print("counter", counter, "current", current, "start", l[0], "end", l[1])
-        counter += l[1] - l[0] + 1
-        current = l[1]
-    elif l[1] >= current:
-        print("counter", counter, "current", current, "start", l[0], "end", l[1])
-        counter += l[1] - current
-        current = l[1]
-    else:
-        print("counter", counter, "current", current, "start", l[0], "end", l[1])
+    newlist = l
+    print("new", newlist, "curr", clist)
+    if newlist[0] >= clist[0] and newlist[0] <= clist[1]:
+        if newlist[1] > clist[1]:
+            clist[1] = newlist[1]
+    if newlist[0] > clist[1]:
+        counter.append(clist)
+        print("append success")
+        clist = newlist
+
+counter.remove([0, 0])
+counter.append(clist)
+
+for c in counter:
+    fcount += c[1] - c[0] + 1
 
 
-print("final answer:", counter)
+print("final answer:", fcount)
