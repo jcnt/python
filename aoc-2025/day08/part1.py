bb99bd42557d54516286fbff645c6630fa00216a
Jacint Juhasz <yyaazz@gmail.com>
2026-02-05 19:19:12 +0100

    Lazy commit Thu Feb  5 19:19:12 CET 2026

--- aoc-2025/day08/part1.py
+++ aoc-2025/day08/part1.py
@@ -3,9 +3,11 @@ Advent of Code, 2025. Day 8, part 1.
 
 """
 
+import math
+
 source = []
 dist_db = {}
-conn_pairs = []
+conn_pairs = [[0, 0]]
 
 with open("example", "r") as file:
     for line in file:
@@ -13,7 +15,7 @@ with open("example", "r") as file:
 
 
 def get_distance(a, b):
-    return (
+    return math.sqrt(
         (int(a[0]) - int(b[0])) ** 2
         + (int(a[1]) - int(b[1])) ** 2
         + (int(a[2]) - int(b[2])) ** 2
@@ -28,9 +30,18 @@ for i in range(0, len(source)):
         distance = get_distance(source[i], source[j])
         dist_db[f"{i},{j}"] = distance
 
-print(dist_db)
 sdict = sorted(dist_db.items(), key=lambda item: item[1])
+print(sdict)
 
 for k in sdict:
     a = int(k[0].split(",")[0])
     b = int(k[0].split(",")[1])
+    print(a, b)
+    for i in range(len(conn_pairs)):
+        if a in conn_pairs[i]:
+            conn_pairs[i].append(b)
+        if b in conn_pairs[i]:
+            conn_pairs[i].append(a)
+        else:
+            conn_pairs.append([a, b])
+    print(conn_pairs)
